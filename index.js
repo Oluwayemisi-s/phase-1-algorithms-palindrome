3439d885755dcc0db1fbb76ae63e9d5cdb31c027
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-10-02 22:44:07 -0700

    commit tree bb3930dc8cbbf5eacb3debe5610d359b13e5d32d

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    const checkPalindrome = word 
+    let halfWord
+    if (checkPalindrome.length === 0){
+        console.log ('No word has been provided to check.')
+        return false
+    } else if (checkPalindrome.length === 1){
+        console.log ('Provided word is an automatic palindrome since it has just one character.')
+        return true
+    } else if (checkPalindrome.length % 2 === 0){
+        halfWord = checkPalindrome.length/2
+        //return halfWord
+    } else {
+        halfWord = (checkPalindrome.length - 1)/2
+        //return halfWord
+    }
+
+    //looping through characters
+    for (let x = 0; x <= halfWord; x++){
+      if (checkPalindrome[x] != checkPalindrome.slice(-1-x)[0]){
+          console.log('The entered word is not a palindrome')
+          return false
+      }
+    }
+    console.log('The entered word is a palindrome')
+    return true
 }
 
+//isPalindrome('robot')
 /* 
   Add your pseudocode here
 */
